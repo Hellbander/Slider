14e190eed79a5b016946dfe76f306743ed40daaf
Mihins <mihails.mihins@accenture.com>
2016-05-10 16:50:37 +0300

    changes

--- script.js
+++ script.js
@@ -35,15 +35,13 @@ function moveSlider(direction) {
          i = i + 1;
       }
    } else if (direction === "right") {
-      newLi = last_Li.cloneNode(true);
-	   slider.insertBefore(newLi, first_Li);
-		newLi.style.marginLeft = sld * 2 + "px";
-		first_Li.style.marginLeft = 0;  
+      move_image = 0;
+		first_Li.style.marginLeft = move_image + "px"; 
+	   
    }
 
    switch (direction) {
    case "left":
-      //first_Li.style.marginLeft = move_image + "px";
       first_Li.remove();
       move_image = sld;
       first_Li = document.getElementsByClassName('li-image')[0];
@@ -51,9 +49,11 @@ function moveSlider(direction) {
 		last_Li = document.getElementsByClassName('li-image')[li_count - 1];
       break;
    case "right":
-		move_image = img_width * (-1);
+		newLi = last_Li.cloneNode(true);
+		newLi.style.marginLeft = sld + "px";
+		slider.insertBefore(newLi, first_Li);
+		first_Li.style.marginLeft = 0;  
 		first_Li = document.getElementsByClassName('li-image')[0];
-	   first_Li.style.marginLeft = move_image + "px"; 
       rightDelete();
       break;
    }
@@ -61,7 +61,6 @@ function moveSlider(direction) {
 
 function rightDelete() {
 	 last_Li.remove();
-	 first_Li = document.getElementsByClassName('li-image')[0];
     last_Li = document.getElementsByClassName('li-image')[li_count - 1];
 }
 
